c92a756054d05a52565dc7c18af3c084a92f15bc
jakim54 <james.kim@huffingtonpost.com>
2017-02-03 17:25:25 -0500

    upto num 7

--- Hospital/School.swift
+++ Hospital/School.swift
@@ -2,13 +2,61 @@
 //  School.swift
 //  Hospital
 
+enum Punishment {
+    case severe, medium, light
+}
 
+protocol Payable {
+    func wages() -> Double
+}
 
+extension Payable {
+    func wages() -> Double {
+        return 50_000.00
+    }
+}
 
+protocol TimeOff {
+    var vacationDays: Int { get set }
+    
+    func requestForVacation(days: Int) -> Bool
+}
 
+extension TimeOff {
+    func requestForVacation(days: Int) -> Bool {
+        if vacationDays == 9 {
+            return true
+        } else if vacationDays == 9 && days == 10 {
+            return false
+        } else {
+            return false
+        }
+    }
+    
+    // return days <= vacationDays
+}
 
+protocol Reprimand {
+    func disciplineStudent(scale: Punishment) -> String
+    
+    func callParentDeliveringMessage(scale: Punishment) -> String
+}
 
+protocol Employee {
+    //TODO
+}
 
+struct AdvisoryBoardMember {
+    //TODO
+}
+
+struct Principal {
+    //TODO
+}
+
+struct Teacher {
+    //TODO
+}
 
 
 
