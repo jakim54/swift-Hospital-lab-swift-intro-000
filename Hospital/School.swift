e8970fe4ac1e98cacb5c06c56ef958908af0ca06
jakim54 <james.kim@huffingtonpost.com>
2017-02-06 16:50:09 -0500

    num 12, the instructions are weird to me

--- Hospital/School.swift
+++ Hospital/School.swift
@@ -3,40 +3,29 @@
 //  Hospital
 
 enum Punishment {
+    
     case severe, medium, light
 }
 
+enum Subject {
+    
+    case math, science, english
+}
+
 protocol Payable {
+    
     func wages() -> Double
 }
 
-extension Payable {
-    func wages() -> Double {
-        return 50_000.00
-    }
-}
-
 protocol TimeOff {
+    
     var vacationDays: Int { get set }
     
     func requestForVacation(days: Int) -> Bool
 }
 
-extension TimeOff {
-    func requestForVacation(days: Int) -> Bool {
-        if vacationDays == 9 {
-            return true
-        } else if vacationDays == 9 && days == 10 {
-            return false
-        } else {
-            return false
-        }
-    }
-    
-    // return days <= vacationDays
-}
-
 protocol Reprimand {
+    
     func disciplineStudent(scale: Punishment) -> String
     
     func callParentDeliveringMessage(scale: Punishment) -> String
@@ -46,21 +35,62 @@ protocol Employee {
     //TODO
 }
 
-struct AdvisoryBoardMember {
-    //TODO
+protocol Teach {
+    
+    func teachSubject(subject: Subject) -> String
 }
 
-struct Principal {
-    //TODO
+extension Payable {
+    
+    func wages() -> Double {
+        return 50_000.00
+    }
 }
 
-struct Teacher {
-    //TODO
+extension TimeOff {
+    
+    func requestForVacation(days: Int) -> Bool {
+        return days <= vacationDays
+    }
 }
 
+extension Reprimand {
+    
+    func disciplineStudent(scale: Punishment) -> String {
+        switch scale {
+        case .severe:
+            return "We are expelling you from school!"
+        case .medium:
+            return "Why would you think that's a good idea?! You're going to detention."
+        case .light:
+            return "You're to go back to class, don't do it again."
+        }
+    }
+    
+    func callParentDeliveringMessage(scale: Punishment) -> String {
+        switch scale {
+        case .severe:
+            return "We are expelling you from school!"
+        case .medium:
+            return "Why would you think that's a good idea?! You're going to detention."
+        case .light:
+            return "You're to go back to class, don't do it again."
+        }
+    }
+}
 
-
-
-
-
+extension Teach {
+    
+    func teachSubject(subject: Subject) -> String {
+        switch subject {
+        case .math:
+            return "Take out your books please."
+        case .science:
+            return "Time to learn the best subject of all! Science!!"
+        case .english:
+            return "To read or not to read. Everyout take our your english books."
+        }
+    }
+    
+}
 
