26015056b737a113db2bb69d93fca0d0b469a0b6
jajuki13 <khkim1776@gmail.com>
2017-02-06 22:20:40 -0500

    completed

--- Hospital/School.swift
+++ Hospital/School.swift
@@ -8,58 +8,48 @@ enum Punishment {
 }
 
 enum Subject {
-    
     case math, science, english
 }
 
 protocol Payable {
-    
     func wages() -> Double
 }
 
 protocol TimeOff {
-    
     var vacationDays: Int { get set }
     
     func requestForVacation(days: Int) -> Bool
 }
 
 protocol Reprimand {
-    
     func disciplineStudent(scale: Punishment) -> String
     
     func callParentDeliveringMessage(scale: Punishment) -> String
 }
 
-protocol Employee {
-    //TODO
-}
+protocol Employee: Payable, TimeOff {}
 
 protocol Teach {
-    
     func teachSubject(subject: Subject) -> String
 }
 
 extension Payable {
-    
     func wages() -> Double {
         return 50_000.00
     }
 }
 
 extension TimeOff {
-    
     func requestForVacation(days: Int) -> Bool {
         return days <= vacationDays
     }
 }
 
 extension Reprimand {
-    
     func disciplineStudent(scale: Punishment) -> String {
         switch scale {
         case .severe:
-            return "We are expelling you from school!"
+            return "We are expelling you from the school!"
         case .medium:
             return "Why would you think that's a good idea?! You're going to detention."
         case .light:
@@ -70,25 +60,24 @@ extension Reprimand {
     func callParentDeliveringMessage(scale: Punishment) -> String {
         switch scale {
         case .severe:
-            return "We are expelling you from school!"
+            return "Your child has been expelled from the school."
         case .medium:
-            return "Why would you think that's a good idea?! You're going to detention."
+            return "Your child has become a terror in the classroom."
         case .light:
-            return "You're to go back to class, don't do it again."
+            return "Your child thought it was funny to put gum in Amy's hair."
         }
     }
 }
 
 extension Teach {
-    
     func teachSubject(subject: Subject) -> String {
         switch subject {
         case .math:
-            return "Take out your books please."
+            return "Take out your math books please."
         case .science:
             return "Time to learn the best subject of all! Science!!"
         case .english:
-            return "To read or not to read. Everyout take our your english books."
+            return "To read or not to read. Everyone take out your english books."
         }
     }
     
--- Hospital/SchoolEmployees.swift
+++ Hospital/SchoolEmployees.swift
@@ -11,12 +11,23 @@ struct AdvisoryBoardMember {
     }
 }
 
-extension AdvisoryBoardMember: Payable, TimeOff {}
+extension AdvisoryBoardMember: Employee {}
 
+struct Principal {
+    var name: String
+    var vacationDays: Int = 20
+    
+    func wages() -> Double {
+        return 80_000.00
+    }
+}
 
+extension Principal: Employee, Reprimand {}
 
+struct Teacher {
+    var name: String
+    var vacationDays: Int = 15
+}
 
-
-
-
+extension Teacher: Employee, Teach {}
 
